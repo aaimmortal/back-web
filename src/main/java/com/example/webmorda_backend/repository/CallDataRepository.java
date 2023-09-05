57c1e18c6171c0308a3f37c5abc003a1491008f5
rustemov4 <azamat.rustemov.02@mail.ru>
2023-09-05 12:49:15 +0600

    new com

--- src/main/java/com/example/webmorda_backend/repository/CallDataRepository.java
+++ src/main/java/com/example/webmorda_backend/repository/CallDataRepository.java
@@ -14,7 +14,7 @@ import java.util.List;
 public interface CallDataRepository extends JpaRepository<CallData, String> {
     List<CallData> getCallDataByCalldateBetween(LocalDateTime dateTime, LocalDateTime dateTime2);
 
-    @Query("SELECT new com.example.webmorda_backend.model.DispositionCount(c.disposition,COUNT(*)) FROM CallData c WHERE c.calldate between :startDate AND :endDate GROUP BY c.disposition")
+    @Query("SELECT new com.example.webmorda_backend.model.DispositionCount(c.disposition,COUNT(*)) FROM CallData c WHERE c.calldate between ?1 AND ?2 GROUP BY c.disposition")
     List<DispositionCount> getCountByDisposition(LocalDateTime startDate, LocalDateTime endDate);
 
     CallData getCallDataByUniqueid(String uniqueid);
